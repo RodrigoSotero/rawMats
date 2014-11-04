13c7fa8fc65e49852889ac0ad68f15d726a8d6ed
Irving <Irving@LGp714.mshome.net>
2014-11-04 12:41:16 -0600

    IRVING

--- rawMats/src/Controlador/Main.java
+++ rawMats/src/Controlador/Main.java
@@ -6,10 +6,28 @@
 
 package Controlador;
 
+import Vista.Splash;
+
 /**
  *
  * @author Irving
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            System.out.println(ex.getMessage());
+        }
+        new jControlador( new Splash() ).iniciar();
+        //Vista.hilobd a =new Vista.hilobd("A");
+        //a.run();
+        
+    }
 }
--- rawMats/src/Controlador/jControlador.java
+++ rawMats/src/Controlador/jControlador.java
@@ -6,10 +6,41 @@
 
 package Controlador;
 
+import Vista.HiloProgreso;
+import Vista.Splash;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Irving
  */
 public class jControlador {
+    HiloProgreso hilo;
+    private static  Vista.Splash splash = new Splash();  
     
+    
+    public jControlador( JFrame padre ){
+        //this.frmprincipal = (frmPrincipal) padre;
+        this.splash = (Splash) padre;
+    }
+    public void iniciar(){
+        Toolkit.getDefaultToolkit().setLockingKeyState(KeyEvent.VK_CAPS_LOCK, true);
+        hilo = new HiloProgreso(this.splash.progreso);
+        hilo.start();
+        hilo = null;
+        this.splash.setLocationRelativeTo(null);
+        this.splash.setVisible(true);
+        this.splash.progreso.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                if (splash.progreso.getValue()==100){
+                    splash.dispose();
+                    //login.setLocationRelativeTo(null);
+                    //login.setVisible(true);
+                    //login.setDefaultCloseOperation(0);
+                }
+            }
+        });
+    }
 }
