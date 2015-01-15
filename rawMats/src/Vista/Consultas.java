5220b0ffecf4d87a9b39a314c45e4b798378c794
Rodrigo <Rodrigo@Rodrigo>
2015-01-15 12:19:46 -0600

    Revert "Revert "rodrigo""
    This reverts commit 835c9e61cdb0eb2de9b2ecb8392c1796c7f9a6f6.

--- rawMats/src/Vista/Consultas.java
+++ rawMats/src/Vista/Consultas.java
@@ -44,6 +44,7 @@ public class Consultas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        __btngrp = new javax.swing.ButtonGroup();
         jPanel9 = new javax.swing.JPanel();
         __optSalida = new javax.swing.JRadioButton();
         __optNinguno = new javax.swing.JRadioButton();
@@ -55,7 +56,7 @@ public class Consultas extends javax.swing.JFrame {
         __etqFechaConMov = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
         __chkDescripcion = new javax.swing.JCheckBox();
-        __descrpcion = new javax.swing.JTextField();
+        __Descripcion = new javax.swing.JTextField();
         __chkProveedor = new javax.swing.JCheckBox();
         __chkTipoEntrada = new javax.swing.JCheckBox();
         __chkTipoSalida = new javax.swing.JCheckBox();
@@ -125,13 +126,16 @@ public class Consultas extends javax.swing.JFrame {
         jPanel9.setBorder(javax.swing.BorderFactory.createTitledBorder("Tipo de Movimiento:"));
         jPanel9.setOpaque(false);
 
+        __btngrp.add(__optSalida);
         __optSalida.setForeground(new java.awt.Color(255, 255, 255));
         __optSalida.setText("Salida");
 
+        __btngrp.add(__optNinguno);
         __optNinguno.setForeground(new java.awt.Color(255, 255, 255));
         __optNinguno.setSelected(true);
         __optNinguno.setText("Ninguno");
 
+        __btngrp.add(__optEntrada);
         __optEntrada.setForeground(new java.awt.Color(255, 255, 255));
         __optEntrada.setText("Entrada");
 
@@ -197,8 +201,8 @@ public class Consultas extends javax.swing.JFrame {
             }
         });
 
-        __descrpcion.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-        __descrpcion.setEnabled(false);
+        __Descripcion.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        __Descripcion.setEnabled(false);
 
         __chkProveedor.setFont(new java.awt.Font("Papyrus", 0, 14)); // NOI18N
         __chkProveedor.setForeground(new java.awt.Color(255, 255, 255));
@@ -1003,6 +1007,7 @@ public class Consultas extends javax.swing.JFrame {
     public javax.swing.JTextField __Area;
     public javax.swing.JTextField __Cantidad;
     public javax.swing.JTextField __Cliente;
+    public javax.swing.JTextField __Descripcion;
     public javax.swing.JMenu __Edicion;
     public javax.swing.JTextField __Maquina;
     public javax.swing.JMenuItem __MenuAbrirArchivo;
@@ -1013,6 +1018,7 @@ public class Consultas extends javax.swing.JFrame {
     public javax.swing.JTextField __TipoEntrada1;
     public javax.swing.JTextField __TipoSalida;
     public javax.swing.JTextField __Ubicacion;
+    private javax.swing.ButtonGroup __btngrp;
     public javax.swing.JCheckBox __chkArea;
     public javax.swing.JCheckBox __chkCantidad;
     public javax.swing.JCheckBox __chkClave;
@@ -1042,7 +1048,6 @@ public class Consultas extends javax.swing.JFrame {
     public javax.swing.JTextField __costo;
     public com.toedter.calendar.JDateChooser __dateIni;
     public com.toedter.calendar.JDateChooser __datefin;
-    public javax.swing.JTextField __descrpcion;
     public javax.swing.JTextField __documento;
     public javax.swing.JLabel __etqFechaConMov;
     public javax.swing.JTextField __folio;
